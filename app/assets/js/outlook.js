a39a6df089b62f056bd6bb15a15bee974b73aea4
Alex Ulianets <oleksandr.ulianets@hotschedules.com>
2017-08-30 14:11:36 +0300

    OPF - 000 Створити структуру проекта на англуярі, MVC, зробити підключення до бекенда.

---
+++ app/assets/js/outlook.js
@@ -0,0 +1,7 @@
+var app = angular.module('main', ['ui.router', 'oc.lazyLoad']);
+
+// Enter page
+
+  app.controller('outlookCtrl', function($scope) {
+  	console.log("outlookCtrl");
+  });
---
+++ app/assets/js/today.js
@@ -0,0 +1,7 @@
+var app = angular.module('main', ['ui.router', 'oc.lazyLoad']);
+
+// Enter page
+
+  app.controller('todayCtrl', function($scope) {
+  	console.log("todayCtrl");
+  });
