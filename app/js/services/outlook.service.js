94a7538a68f7a4b73c26750f5281e11bee7230d7
Alex Ulianets <oleksandr.ulianets@hotschedules.com>
2017-09-02 21:32:15 +0300

    OPF - 002 outlook.html - Weekly weather report

--- app/js/services/outlook.service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var services = angular.module('oplaoApp.services', ['ngResource']);
-
-services.factory('DummyFactory', function ($resource) {
-    return $resource('/', {}, {
-        query: { method: 'GET', params: {} }
-    })
-});
