d8ec9bd4ba6f1e08ae2acd92368e2b41c5f49946
Vadagon <l3adagon@gmail.com>
2017-09-15 23:08:36 +0300

    OPF-012 search + last_visited_cities FRONT END

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -6,14 +6,60 @@ var app = angular.module('main', ['ui.router', 'oc.lazyLoad', 'ngCookies']);
         $rootScope.example = "";
         $rootScope.local = {};
         $rootScope.local.typeTemp = 'C';
+        $rootScope.searchInput = '';
+        $rootScope.searchList = [];
 
-        $.ajax({
+        $rootScope.get_recent_cities_tabs_func = function(){
+            $.ajax({
+                method: "POST",
+                url: "/get_recent_cities_tabs"
+            }).done(function( msg ) {
+                $rootScope.get_recent_cities_tabs = msg;
+                console.log(msg);
+                $rootScope.$apply;
+            });
+        }
+
+        $rootScope.get_api_weather = function(){
+            $.ajax({
+                method: "POST",
+                url: "/get_api_weather"
+            }).done(function( msg ) {
+                $rootScope.temperature = msg;
+                $rootScope.get_recent_cities_tabs_func();
+            })
+        }
+        $rootScope.get_api_weather();
+
+        $rootScope.searchHint = function(){
+                console.log($rootScope.searchInput);
+                if($rootScope.searchInput.length > 1){
+                     $.ajax({
+                        method: "POST",
+                        url: "/find_occurences/"+$rootScope.searchInput
+                     }).done(function( msg ) {
+                        console.log(msg);
+                        $rootScope.searchList = msg;
+                        $rootScope.$apply;
+                     })
+                }else{
+                      $rootScope.searchList = $rootScope.get_recent_cities_tabs;
+                      console.log($rootScope.searchList);
+                      $rootScope.$apply;
+
+                }
+        }
+        $rootScope.selectCity = function(e){
+            console.log(e);
+            $.ajax({
             method: "POST",
-            url: "/get_api_weather"
-        }).done(function( msg ) {
-            $rootScope.temperature = msg;
-        })
-
+                url: "/select_city/"+e
+            }).done(function( msg ) {
+                console.log(msg)
+                $state.reload();
+                $rootScope.get_api_weather();
+            })
+        }
         $rootScope.updateTemp = function(){
             readyGet($rootScope.$$childHead.detailedTemp, $rootScope.$$childHead.get_year_summary, $rootScope.local.typeTemp)
         }
