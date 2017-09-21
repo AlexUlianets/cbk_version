21e477ac1f3ef3482307d4fffae0b45c3e9a60e0
vladik0555 <vladikabcdeg@gmail.com>
2017-09-21 17:35:04 +0300

    fixed bugs release 0.1

--- src/main/java/com/oplao/model/DetailedForecastGraphMapping.java
+++ src/main/java/com/oplao/model/DetailedForecastGraphMapping.java
@@ -3,16 +3,26 @@ package com.oplao.model;
 public class DetailedForecastGraphMapping extends TempMapping{
 
     private String date;
-    private double precip;
+    private double precipMM;
+    private double precipInch;
     private String weatherIcon;
 
-    public DetailedForecastGraphMapping(int tempC, int tempF, String date, double precip, String weatherIcon) {
+    public DetailedForecastGraphMapping(int tempC, int tempF, String date, double precipMM, double precipInch, String weatherIcon) {
         super(tempC, tempF);
         this.date = date;
-        this.precip = precip;
+        this.precipMM = precipMM;
+        this.precipInch = precipInch;
         this.weatherIcon = weatherIcon;
     }
 
+
+    public double getPrecipInch() {
+        return precipInch;
+    }
+
+    public void setPrecipInch(double precipInch) {
+        this.precipInch = precipInch;
+    }
 
     public String getWeatherIcon() {
         return weatherIcon;
@@ -46,11 +56,11 @@ public class DetailedForecastGraphMapping extends TempMapping{
         this.date = date;
     }
 
-    public double getPrecip() {
-        return precip;
+    public double getPrecipMM() {
+        return precipMM;
     }
 
-    public void setPrecip(double precip) {
-        this.precip = precip;
+    public void setPrecipMM(double precipMM) {
+        this.precipMM = precipMM;
     }
 }
